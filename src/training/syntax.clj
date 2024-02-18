4db759f6e10b451d5d66ded15af7d7e2d98b7763
Jason Robinson <jasonsrobinson@protonmail.com>
2024-02-18 15:24:59 -0500

    Chapter 3 training

--- src/training/syntax.clj
+++ src/training/syntax.clj
@@ -17,4 +17,23 @@
   (prn g t)
   (str greeting "-" tone))
 
+;; 3.2 Pre- and post-conditions
+(defn f
+  "Using pre- and post-conditions.
+  Advantages: succinct (also similar to Design by Contract)
+  Disadvantages: Assertions can be disabled, less control over error description
+  and handling"
+  [x]
+  {:pre [(pos? x)]
+   :post [(neg? %) (int? %)]}
+  (- x))
 
+(defn g
+  "Check for a condition and throw an exception"
+  [x]
+  (when-not (pos? x)
+    (throw (ex-info "bad input" {:x x})))
+  (let [result (- x)]
+    (if (and (neg? result) (int? result))
+      result
+      (throw (ex-info "bad result" {:x x})))))
