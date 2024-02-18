95607aeeecc467c63bb378d5e07e39c48fc71997
Jason Robinson <jasonsrobinson@protonmail.com>
2024-02-18 11:29:30 -0500

    Training with Namespaces and Regex

--- src/training/core.clj
+++ src/training/core.clj
@@ -1,4 +1,6 @@
-(ns training.core)
+(ns training.core
+  (:require [clojure.string :as string])
+  (:import [java.util Date]))
 
 (+ 1 2 3)
 
@@ -6,3 +8,12 @@
 
 ;; Calculate 5!
 (reduce * (range 1 6))
+
+;; 2.7 Namespaces
+(string/upper-case "shout")
+
+;; 2.8 Regex
+(re-seq #"\w+" "the quick brown fox")
+
+
+
